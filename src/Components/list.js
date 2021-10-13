e748434c1fc20ee44b1b83b1090f3015a5e2b195
Stéphanie Antignac <antignacstephanie17@gmail.com>
2021-10-13 16:23:17 +0200

    Key ok + useState

--- src/Components/list.js
+++ src/Components/list.js
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
+
 export default function List(){
+    const initialTodos = ["Learn React", "Add Some Effects", "Third things to do"];
+    const [todos, setTodos] = useState(initialTodos);
+    console.log(setTodos);
+    
+    const itemTodo = todos.map((todo)=> (
+        <li className="list__item" key={todo}><input type="checkbox"/>{todo}</li>
+      ));
+    
     return(
         <>
         <div className="list">
             <h2>Todos</h2>
             <ul className="list__ul">
-            <li className="list__item"><input type="checkbox"/>Learn React</li>
-            <li className="list__item"><input type="checkbox"/>Add some effect</li>
-            <li className="list__item"><input type="checkbox"/>Add some effect</li>
-            <li className="list__item"><input type="checkbox"/>Add some effect</li>
+                {itemTodo}
             </ul>
         </div>
         </>
