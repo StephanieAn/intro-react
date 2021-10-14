7f12e4564641028f821b6327ad33d7d1c8198943
Stéphanie Antignac <antignacstephanie17@gmail.com>
2021-10-14 16:26:13 +0200

    Début Day 3: some changes not much

--- src/Components/form.js
+++ src/Components/form.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function Form(){
+    const inputRef = useRef();
+    const clickHandler = () => {
+        const inputElement = inputRef.current;
+
+        console.log(inputElement.value);
+    }
     return(
         <>
             <form className="todo__form">
-                <input placeholder="Type a new todo" />
-                <button type="submit">Add Todo</button>
+                <input ref={inputRef} type="text" placeholder="Type a new todo" />
+                <button type="submit" onClick={clickHandler}>Add Todo</button>
             </form>
         </>
     )
